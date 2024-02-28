8f0624736e6c662dd149a2e02e0f1c5e3fbaa197
Callum Hackett <callumjhackett@gmail.com>
2024-02-28 21:58:45 +0000

    search test

--- corpora.py
+++ corpora.py
@@ -6,7 +6,9 @@ with open("hotpot_train_v1.1.json") as f:
     data = json.load(f)
 """
 
-with open("hotpot_train_v1.1_questions.txt", encoding="utf-8") as questions:
-    data = questions.read()
+with open("hotpot_train_v1.1_questions.txt", encoding="utf-8") as data:
+    questions = [line.rstrip() for line in data]
 
-st.write(data[:100])
+for q in questions:
+    if "Michael" in q:
+        st.write(q)
