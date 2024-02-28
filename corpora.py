430785f5fe4b9407b82cb5f2198db361ae84b54e
Callum Hackett <callumjhackett@gmail.com>
2024-02-28 22:00:56 +0000

    interaction test

--- corpora.py
+++ corpora.py
@@ -9,6 +9,8 @@ with open("hotpot_train_v1.1.json") as f:
 with open("hotpot_train_v1.1_questions.txt", encoding="utf-8") as data:
     questions = [line.rstrip() for line in data]
 
+query = st.text_input('Search term: ')
+
 for q in questions:
-    if "Michael" in q:
+    if query in q:
         st.write(q)
