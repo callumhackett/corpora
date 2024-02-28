406cfc3b274f573e0e9b3094633a6d48ac7a3a4e
Callum Hackett <callumjhackett@gmail.com>
2024-02-28 23:16:19 +0000

    aesthetic changes

--- corpora.py
+++ corpora.py
@@ -1,20 +1,20 @@
 import json
 import streamlit as st
 
-st.markdown("# Corpus Search")
+st.markdown("### Corpus Search")
 
 # to do: change sources and present them as something other than dropdown list
 # to do: have associated choice for max number of results per source
-sources = st.multiselect("Data sources:", ["Questions", "Contexts"])
+sources = st.multiselect("Data sources:", ["HotpotQA Questions", "HotpotQA Contexts"])
 
 with open("hotpot_train_v1.1_questions.txt", encoding="utf-8") as data:
     questions = [line.rstrip() for line in data]
 
-query = st.text_input('Search term: ')
+query = st.text_input("Search term:")
 
 if "Questions" in sources:
 
-    st.markdown("# Questions Results")
+    st.markdown("#### Questions Results")
 
     for q in questions:
         if query != "" and query.lower() in q.lower():
