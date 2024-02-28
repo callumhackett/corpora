fddd4c375e2f7bd0a82be2c788005706df733d2f
Callum Hackett <callumjhackett@gmail.com>
2024-02-28 22:22:09 +0000

    source multiselect

--- corpora.py
+++ corpora.py
@@ -3,11 +3,21 @@ import streamlit as st
 
 st.markdown("# Corpus Search")
 
+sources = st.multiselect("Data sources:", ["Questions", "Contexts"])
+
 with open("hotpot_train_v1.1_questions.txt", encoding="utf-8") as data:
     questions = [line.rstrip() for line in data]
 
 query = st.text_input('Search term: ')
 
-for q in questions:
-    if query != "" and query.lower() in q.lower():
-        st.markdown(q.replace(query, "**"+query+"**"))
+if "Questions" in sources:
+
+    st.markdown("# Questions Results")
+
+    for q in questions:
+        if query != "" and query.lower() in q.lower():
+            st.markdown(q.replace(query, "**"+query+"**"))
+
+if "Contexts" in sources:
+
+    st.write("Context data not ready yet")
