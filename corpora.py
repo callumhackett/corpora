808bd961d29c1d28107e31968357c6312b3c634e
Callum Hackett <callumjhackett@gmail.com>
2024-02-28 22:11:27 +0000

    markdown magic

--- corpora.py
+++ corpora.py
@@ -9,8 +9,8 @@ with open("hotpot_train_v1.1.json") as f:
 with open("hotpot_train_v1.1_questions.txt", encoding="utf-8") as data:
     questions = [line.rstrip() for line in data]
 
-query = st.text_input('Search term: ')
+query = st.text_input('Search term: ').lower()
 
 for q in questions:
-    if query != "" and query in q:
-        st.write(q)
+    if query != "" and query in q.lower():
+        st.markdown(q.replace(query, "**"+query+"**"))
