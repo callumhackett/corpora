a660afc3d1c1a4c465e5d2d2e024244173bb9939
massimo-lu <153612777+massimo-lu@users.noreply.github.com>
2024-02-29 00:37:43 +0000

    Stats (#1)
    * Update README.md
    
    * turn query into regex, keep original casing when highlighting, match only whole words (i.e. not substrings)
    
    * added case toggle
    
    * added case toggle
    
    * match wildcard * as regex \w+, use yellow highlighting, report number of hits
    
    * make toggle case insensitive default, use underlining instead of highlight
    
    * added tables of hit counts, absolute counts instead of doc counts
    
    * fix col width
    
    ---------
    
    Co-authored-by: massimo-lu <massimo@git.hub>
    Co-authored-by: Callum Hackett <79424607+callumhackett@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # corpora
 
-test
+test test
--- corpora.py
+++ corpora.py
@@ -1,4 +1,7 @@
+from collections import Counter
 import json
+import re
+import pandas as pd
 import streamlit as st
 
 st.markdown("### Corpus Search")
@@ -10,16 +13,56 @@ sources = st.multiselect("Data sources:", ["HotpotQA Questions", "HotpotQA Conte
 with open("hotpot_train_v1.1_questions.txt", encoding="utf-8") as data:
     questions = [line.rstrip() for line in data]
 
-query = st.text_input("Search term:")
+num_questions = len(questions)
 
-if "Questions" in sources:
+case_sensitive = st.toggle('Case sensitive')
+case_flag = re.IGNORECASE if not case_sensitive else 0
 
-    st.markdown("#### Questions Results")
+query = st.text_input('Search term: ').strip().replace("*", "\w+")
+query_re = re.compile(r"\b" + query + r"\b", flags=case_flag)
 
-    for q in questions:
-        if query != "" and query.lower() in q.lower():
-            st.markdown(q.replace(query, f"<ins>{query}</ins>"))
+if query:
+    if "Questions" in sources:
 
-if "Contexts" in sources:
+        matches = []
+        match_count = 0
 
-    st.write("Context data not ready yet")
+        # Find the matches
+        for q in questions:
+            match = re.findall(query_re, q)
+            if query != "" and match:
+                match_count += len(match)
+                matches.append((match, q))
+
+        # Get some stats
+        text_hits, _ = zip(*matches)
+        all_hits = [x for xs in text_hits for x in xs]
+        if not case_sensitive:
+            all_hits = [t.lower() for t in all_hits]
+        hit_counts = Counter(all_hits)
+        num_unique_hits = len(hit_counts)
+        hit_df = pd.DataFrame(
+            {"Hit": hit_counts.keys(), "Count": hit_counts.values()}
+        ).sort_values(by=["Count", "Hit"], ascending=False).reset_index(drop=True)
+
+        st.markdown(f"Unique hits: {len(hit_df)}")
+        st.dataframe(hit_df, column_config={"Hit": st.column_config.TextColumn()})
+        st.dataframe(hit_df["Count"].describe().to_frame().transpose()[["mean", "std", "min", "max"]])
+
+        # Underscore the hits
+        st.markdown(f"### Questions Results ({match_count})")
+        for hits, original_str in matches:
+            for single_hit in hits:
+                original_str = re.sub(
+                    single_hit,
+                    "<u>"+single_hit+"</u>",
+                    original_str
+                )
+            st.markdown(
+                original_str,
+                unsafe_allow_html=True
+            )
+
+    if "Contexts" in sources:
+
+        st.write("Context data not ready yet")
