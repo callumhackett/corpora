65ddb49af009bef16c15e1607d8a2c4c4d605213
Callum Hackett <callumjhackett@gmail.com>
2024-02-29 04:38:02 +0000

    new data

--- corpora.py
+++ corpora.py
@@ -1,3 +1,4 @@
+from collections import Counter
 import json
 import re
 import pandas as pd
@@ -19,47 +20,39 @@ def compile_data(source):
 
 def find_matches(query, case_flag, data):
     query_re = re.compile(r"\b" + query + r"\b", flags=case_flag)
-    matches = []
+    match_counts = Counter()
+    match_contexts = []
     for entry in data:
         match = re.findall(query_re, entry)
         if match:
-            matches.append((match, entry))
-    return matches
+            for m in match:
+                match_counts[m] += 1
+            match_contexts.append(entry)
+    return match_counts, match_contexts
 
 # User Interface
 st.markdown("### Corpus Search")
 source = st.radio("**Data source**:", DATASETS)
 data = compile_data(source)
-query = st.text_input("Search term (use * as a wildcard):").strip().replace("*", "(\w|-)+")
+query = st.text_input("**Search term (use * as a wildcard):**").strip().replace("*", "(\w|-)+")
 case_sensitive = st.toggle("case-sensitive")
 case_flag = re.IGNORECASE if not case_sensitive else 0
 
 if query != "":
     # matches
-    matches = find_matches(query, case_flag, data)
+    match_counts, match_contexts = find_matches(query, case_flag, data)
+    match_strings = len(match_counts)
 
     # stats
-    match_counts = Counter(matches)
-    unique_match_count = len(match_counts)
     hit_df = pd.DataFrame(
         {"Hit": match_counts.keys(), "Count": match_counts.values()}
     ).sort_values(by=["Count", "Hit"], ascending=False).reset_index(drop=True)
 
     st.markdown(f"Unique hits: {len(hit_df)}")
     st.dataframe(hit_df, column_config={"Hit": st.column_config.TextColumn()})
-    st.dataframe(hit_df["Count"].describe().to_frame().transpose()[["mean", "std", "min", "max"]])
 
     # results
-    st.markdown(f"#### Results ({len(matches_compiled)})")
+    st.markdown(f"#### Results ({sum(match_counts.values())})")
     
-    for hits, original_str in matches:
-        for single_hit in hits:
-            original_str = re.sub(
-                single_hit,
-                "<u>"+single_hit+"</u>",
-                original_str
-            )
-        st.markdown(
-            original_str,
-            unsafe_allow_html=True
-        )
+    for context in match_contexts:
+        st.write(context)
