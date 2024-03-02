5fa2eade02ed7f5af94f279831faed3437b4cbf1
Callum Hackett <callumjhackett@gmail.com>
2024-03-02 20:15:26 +0000

    visual improvements

--- corpora.py
+++ corpora.py
@@ -16,13 +16,15 @@ def compile_data(source):
     Source is chosen by user in Search Parameters.
     """
     data = []
+    word_count = 0
     source = ("_").join(source.lower().split())
 
     with open(os.path.join("data", f"{source}.txt"), encoding="utf-8") as f:
         for line in f:
             data.append(line)
+            word_count += len(line.split())
 
-    return data
+    return data, len(data), word_count
 
 def find_matches(query, data):
     """Find, count and return all token matches for a RegEx in each item of a list of strings."""
@@ -85,37 +87,46 @@ with statistics:
 if query != "":
     # search
     query = re.compile(r"\b" + query.replace("*", "[\w|-]+") + r"\b", flags=case_flag) # convert text query to regex
-    data = compile_data(source) # compile data from user-chosen source
-    dataset_size = len(data) # measure data size for stats
-    token_counts, entry_counts, entry_texts = find_matches(query, data) # extract search results
+    source_data, source_entry_count, source_word_count = compile_data(source) # compile data from user-chosen source
+    token_counts, entry_counts, entry_texts = find_matches(query, source_data) # extract search results
     token_total = sum(token_counts.values())
 
     # display results
     with results:
-        if len(entry_texts) == MAX_RETURNS:
-            st.markdown(f"Because of a large number of matches, the displayed results have been capped at {MAX_RETURNS:,}")
-        results_table_container = st.container(height=500, border=False) # place results inside fixed-height container
-    with results_table_container.container():
-        results_table = pd.DataFrame({"": entry_texts}) # convert search results to table
-        results_table.index += 1 # set row index to start from 1 instead of 0
-        st.markdown( # convert pd table to HTML to allow text highlighting
-            results_table.to_html(escape=False, header=False, bold_rows=False), unsafe_allow_html=True
-        )
+        if not entry_texts:
+            st.markdown("There were no results for your search.")
+        else:
+            if len(entry_texts) == MAX_RETURNS:
+                st.markdown(f"Because of a large number of matches, the displayed results have been capped at {MAX_RETURNS:,}")
+            results_table_container = st.container(height=500, border=False) # place results inside fixed-height container
+            with results_table_container.container():
+                results_table = pd.DataFrame({"": entry_texts}) # convert search results to table
+                results_table.index += 1 # set row index to start from 1 instead of 0
+                st.markdown( # convert pd table to HTML to allow text highlighting
+                    results_table.to_html(escape=False, header=False, bold_rows=False), unsafe_allow_html=True
+                )
 
     # display stats
     with statistics:
         if entry_texts:
-            entry_proportion = round(100*(entry_counts/dataset_size), 2) or "<0.01"
-            st.markdown(f"{entry_proportion}% of entries in your source had ≥1 match.")
+            entry_proportion = round(100*(entry_counts/source_entry_count), 2) or "<0.01"
+            token_proportion = round(100*(token_total/source_word_count), 2) or "<0.01"
+            st.markdown(
+                f"""
+                - Entries with ≥1 match: {entry_proportion}%
+                - Unique matches: {len(token_counts.keys()):,}
+                - Total matches: {token_total:,}
+                - Source text covered: {token_proportion}%
+                """
+            )
+            stats_table = pd.DataFrame( # convert string match data to table
+                {"string": token_counts.keys(),
+                "count": token_counts.values(),
+                "% in set": [round(100*(value/token_total), 2) or "<0.01" for value in token_counts.values()]}
+            ).sort_values(by=["count", "string"], ascending=False).reset_index(drop=True)
+            stats_table.index += 1 # set row index to start from 1 instead of 0
+            st.dataframe(
+                stats_table, column_config={"Hit": st.column_config.TextColumn()}, use_container_width=True
+            )
             if source == "HotpotQA Contexts":
-                st.markdown("Each of the multiple contexts per HotpotQA question is counted as one entry.")
-            st.markdown(f"There was a total of {token_total:,} match token(s):")
-        stats_table = pd.DataFrame( # convert string match data to table
-            {"string": token_counts.keys(),
-             "count": token_counts.values(),
-             "% in set": [round(100*(value/token_total), 2) or "<0.01" for value in token_counts.values()]}
-        ).sort_values(by=["count", "string"], ascending=False).reset_index(drop=True)
-        stats_table.index += 1 # set row index to start from 1 instead of 0
-        st.dataframe(
-            stats_table, column_config={"Hit": st.column_config.TextColumn()}, use_container_width=True
-        )
+                st.caption("Each of the multiple contexts per HotpotQA question is counted as one entry.")
