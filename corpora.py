878877266bb99d986b0840c35b6108fa7c08f259
Callum Hackett <callumjhackett@gmail.com>
2024-03-08 22:31:09 +0000

    refactoring

--- corpora.py
+++ corpora.py
@@ -4,40 +4,40 @@ import re
 import pandas as pd
 import streamlit as st
 
-MAX_RETURNS = 1000 # maximum number of match entries to show in the results table
+CACHE_SIZE = 1 # number of compiled corpora to keep in memory; more than 1 may cause issues over the web
+DATA_FOLDER = "data" # folder with .txt files of line-separated corpus data; filenames will be used in user selection
+MAX_RETURNS = 1000 # maximum number of corpus examples to show in the main results table
 
-st.set_page_config(page_title="Corpora", layout="wide") # browser tab title and page layout
-
-@st.cache_data(max_entries=1) # keep one and only one compiled data source in memory
-def compile_data(source):
+@st.cache_data(max_entries=CACHE_SIZE)
+def compile_corpus(source_name):
     """
-    Compile data from a line-separated .txt source into a list of strings.
-    The source is chosen by the user in Search Parameters.
+    Compile corpus data from a .txt source of line-separated examples into a list of strings.
+    The source is chosen by the user from options in Search Parameters derived from filenames in DATA_FOLDER.
     """
     data = []
     vocab = Counter()
-    source = ("_").join(source.lower().split())
+    source_name = ("_").join(source_name.lower().split())
     punctuation = re.compile(r'[,;!/:\(\)\.\?"\[\]]')
 
-    with open(os.path.join("data", f"{source}.txt"), encoding="utf-8") as f:
+    with open(os.path.join(DATA_FOLDER, f"{source_name}.txt"), encoding="utf-8") as f:
         for line in f:
             data.append(line)
             line = line.replace("-", " ") # separate hyphenations for raw vocab counts
             line = re.sub(punctuation, "", line) # remove punctuation for raw vocab counts
             for word in line.split():
-                if word.isalpha(): # only count words as vocab items
+                if word.isalpha(): # only count alphabetic tokens as vocab items
                     vocab[word.lower()] += 1
 
-    entry_count = len(data) # calculate the number of entries in the source
-    vocab_size = len(vocab) # calculate the number of unique vocab items in the source
-    word_count = sum(vocab.values()) # calculate the number of tokens in the source
+    entry_count = len(data) # calculate the number of entries in the corpus
+    vocab_size = len(vocab) # calculate the number of unique vocab items in the corpus
+    word_count = sum(vocab.values()) # calculate the number of tokens in the corpus
 
     return data, entry_count, vocab, vocab_size, word_count
 
 def find_matches(query, data):
     """
     Find, count and return all token matches for a RegEx in each item of a list of strings.
-    It is assumed that the RegEx does not contain groups.
+    The RegEx should not contain groups (a constraint to allow the interface to be used by non-coders).
     """
     token_counts = Counter()
     entry_count = 0
@@ -53,7 +53,7 @@ def find_matches(query, data):
                     token_counts[m.lower()] += 1
                 else:
                     token_counts[m] += 1
-            # add matches to list for displaying with HTML styling
+            # add matches to list with HTML styling for results table
             if len(entry_texts) < MAX_RETURNS:
                 for m in set(match):
                     entry = re.sub(r"\b" + m + r"\b", '<font color="red"><b>' + m + "</b></font>", entry)
@@ -62,36 +62,26 @@ def find_matches(query, data):
     return token_counts, entry_count, entry_texts
 
 # User Interface
-parameters, results, statistics = st.columns(spec=[0.2, 0.525, 0.275], gap="large")
+st.set_page_config(page_title="Corpora", layout="wide") # browser tab title and page layout; must be first st call
+corpus_filenames = sorted(
+    [file.rstrip(".txt").replace("_", " ") for file in os.listdir(DATA_FOLDER) if file.endswith(".txt")]
+)
+parameters, results, statistics = st.columns(spec=[0.2, 0.525, 0.275], gap="large") # columns with % widths and gap size
 
 # Search Parameters column
 with parameters:
     st.markdown("#### Search Parameters")
-    source = st.radio("**Source**:", [
-        "DROP Questions",
-        "DROP Contexts",
-        "HotpotQA Questions",
-        "HotpotQA Contexts",
-        "QALD-9 Questions",
-        "Spoken English",
-        "SQuAD2.0 Questions",
-        "SQuAD2.0 Contexts"
-    ])
+    source = st.radio("**Source**:", corpus_filenames) # radio buttons for corpus choice
     case_sensitive = st.toggle("case-sensitive")
     case_flag = re.IGNORECASE if not case_sensitive else 0
     query = st.text_input("**Search term**:").strip()
     st.caption(
         """
-        Use * as a wildcard and ^ to match the start of an entry
-        """
-    )
-    st.caption(
-        """
+        Use * as a wildcard and ^ to match the start of an entry\n\n
         *All benchmark data is sourced exclusively from training datasets*
         """
     )
-
-    source_data, source_entry_count, source_vocab, source_vocab_size, source_word_count = compile_data(source)
+    source_data, source_entry_count, source_vocab, source_vocab_size, source_word_count = compile_corpus(source)
 
 # Results column
 with results:
@@ -155,8 +145,8 @@ if query != "":
                 """
             )
     else: # search
-        query = query.replace("*", "[\w|-]+").replace(".", "\.")
-        query_re = re.compile(r"\b" + query + r"\b", flags=case_flag) # convert text query to regex
+        query = query.replace("*", "[\w|-]+").replace(".", "\.") # convert wildcard string to RegEx pattern
+        query_re = re.compile(r"\b" + query + r"\b", flags=case_flag) # compile query as actual RegEx
         token_counts, entry_count, entry_texts = find_matches(query_re, source_data) # extract search results
         token_total = sum(token_counts.values())
 
@@ -167,9 +157,12 @@ if query != "":
             else:
                 if len(entry_texts) == MAX_RETURNS:
                     st.markdown(
-                        f"Because of a large number of matches, the displayed results have been capped at {MAX_RETURNS:,}"
+                        f"""
+                        Because of a large number of matches, the displayed results have been capped at 
+                        {MAX_RETURNS:,}
+                        """
                     )
-                results_table_container = st.container(height=500, border=False) # place results inside fixed-height container
+                results_table_container = st.container(height=500, border=False) # place results in fixed-height box
                 with results_table_container.container():
                     results_table = pd.DataFrame({"": entry_texts}) # convert search results to table
                     results_table.index += 1 # set row index to start from 1 instead of 0
