2058740beef4383a327b95462af526c86a1a2a48
Callum Hackett <callumjhackett@gmail.com>
2024-03-28 00:03:03 +0000

    simplified benchmark import

--- benchmark_import.py
+++ benchmark_import.py
@@ -1,4 +1,5 @@
 import json
+from collections import defaultdict
 
 def import_benchmark_data(
         filepath, benchmark_name,
@@ -8,12 +9,12 @@ def import_benchmark_data(
     """
     Import benchmark data from a JSON file into a list of test cases (each a dict) or a dict of content types (each a list).
 
-    The test case return gives a list of dicts, where each dict can have any of the keys "title", "contexts" and "questions" with lists of titles, contexts and questions per test case. The content type return gives a dict of "titles", "contexts" and "questions", where each has a list for all such content across all test cases.
+    The test case return is a list of dicts, where each dict can have the keys "title", "contexts" and "questions" with lists of titles, contexts and questions per test case. The content type return is a dict with any of "titles", "contexts" and "questions", where each value is a list of all such content across all test cases.
 
     Args:
         filepath: the location of the JSON file
         benchmark_name: one of "drop", "hotpot", "squad2" for individualised processing
-        inclusions: a list with any combination of "titles", "contexts", "questions" (default lists all)
+        inclusions: a list with any of "titles", "contexts", "questions" (default is all)
         organise_by: one of "cases", "content" to return either a list of test cases or a dict of content types (default "cases")
         hotpot_levels: a list with any of "easy", "medium", "hard" to filter by difficulty (default ["hard"])
         hotpot_distractors: a Boolean for whether to include the distractor contexts with hotpot (default False)
@@ -21,62 +22,62 @@ def import_benchmark_data(
     # import the data and initialise the extraction
     with open(filepath, encoding="utf-8") as f:
         source = json.load(f)
-    case_data = []
+    cases = []
+    contents = defaultdict(list)
 
     if benchmark_name == "drop":
         # DROP has the form:
         # {ID:{"passage":CONTEXT, "qa_pairs":[{"question": QUESTION1}, {"question": QUESTION2}, ...]}, ...}
-        for test_case in source.values():
-            extraction = {}
-            if "contexts" in inclusions:
-                extraction["contexts"] = [test_case["passage"].strip()]
-            if "questions" in inclusions:
-                extraction["questions"] = [qa_pair["question"].strip() for qa_pair in test_case["qa_pairs"]]
-            case_data.append(extraction)
+        for source_case in source.values():
+            source_data = {
+                "contexts":[source_case["passage"].strip()],
+                "questions":[qa_pair["question"].strip() for qa_pair in source_case["qa_pairs"]]
+            }
+            if organise_by == "cases":
+                cases.append({inclusion:source_data[inclusion] for inclusion in inclusions})
+            elif organise_by == "content":
+                for inclusion in inclusions:
+                    contents[inclusion].extend(source_data[inclusion])
 
     elif benchmark_name == "hotpot":
         # HotpotQA has the form:
         # [{"level": hard, "question": QUESTION, "context": [[SOURCE_NAME, [SENTENCE, SENTENCE, ...]], ...],
         #   "supporting_facts": [[SOURCE_NAME, FACT_LOCATION_INDEX], ...]}, ...]
-        for test_case in source:
-            # filter by difficulty level
-            if test_case["level"] not in hotpot_levels:
+        for source_case in source:
+            if source_case["level"] not in hotpot_levels:
                 continue
-            extraction = {}
-            if "contexts" in inclusions:
-                # add distractors
-                if hotpot_distractors:
-                    extraction["contexts"] = [" ".join(context[1]) for context in test_case["context"]]
-                # or exclude them
-                else:
-                    supporting_titles = [context_details[0] for context_details in test_case["supporting_facts"]]
-                    extraction["contexts"] = [
-                        " ".join(context[1]) for context in test_case["context"] if context[0] in supporting_titles
-                    ]
-            if "questions" in inclusions:
-                extraction["questions"] = [test_case["question"].strip()]
-            case_data.append(extraction)
+            source_data = {"questions":[source_case["question"].strip()]}
+            if hotpot_distractors:
+                source_data["contexts"] = [" ".join(context[1]) for context in source_case["context"]]
+            else:
+                supporting_titles = [context_details[0] for context_details in source_case["supporting_facts"]]
+                source_data["contexts"] = [
+                    " ".join(context[1]) for context in source_case["context"] if context[0] in supporting_titles
+                ]
+            if organise_by == "cases":
+                cases.append({inclusion:source_data[inclusion] for inclusion in inclusions})
+            elif organise_by == "content":
+                for inclusion in inclusions:
+                    contents[inclusion].extend(source_data[inclusion])
 
     elif benchmark_name == "squad2":
         # SQuAD2.0 has the form:
         # {"data": [{"paragraphs": [{"qas": [{"question": QUESTION}, ...], "context": CONTEXT}, ...]}, ...]}
         source = source["data"]
         for grouping in source:
-            for test_case in grouping["paragraphs"]:
-                extraction = {}
-                if "titles" in inclusions:
-                    extraction["titles"] = grouping["title"]
-                if "contexts" in inclusions:
-                    extraction["contexts"] = [test_case["context"].strip()]
-                if "questions" in inclusions:
-                    extraction["questions"] = [qa_pair["question"].strip() for qa_pair in test_case["qas"]]
-                case_data.append(extraction)
-
-    if organise_by == "content":
-        content_data = {inclusion:[] for inclusion in inclusions}
-        for test_case in case_data:
-            for label, content in test_case.items():
-                content_data[label].extend(content)
-        return content_data
-
-    return case_data
+            for source_case in grouping["paragraphs"]:
+                source_data = {
+                    "titles":[grouping["title"]],
+                    "contexts":[source_case["context"].strip()],
+                    "questions":[qa_pair["question"].strip() for qa_pair in source_case["qas"]]
+                }
+                if organise_by == "cases":
+                    cases.append({inclusion:source_data[inclusion] for inclusion in inclusions})
+                elif organise_by == "content":
+                    for inclusion in inclusions:
+                        contents[inclusion].extend(source_data[inclusion])
+    
+    if organise_by == "cases":
+        return cases
+    
+    return contents
